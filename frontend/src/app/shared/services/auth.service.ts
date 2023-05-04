cb26ae0486b6bceeae6341ddc9c7ca4cb3d624ac
Raja Siddharth Raju <rsrajuofficial@gmail.com>
2023-05-04 17:09:56 +0530

    Checkin with new prompt design and an alert box

--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, tap, throwError } from "rxjs";
 import { URL } from "src/environment/environment";
+import { LocalStorageService } from "./local-storage.service";
 
 interface AuthToken{
     token: string;
@@ -8,8 +10,24 @@ interface AuthToken{
 }
 @Injectable({ providedIn: 'root' })
 export class AuthService{
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient, private localStorageService: LocalStorageService){}
     login(data: FormData){
-        return this.http.post<AuthToken>(`${URL}/login/`, data);
+        return this.http.post<AuthToken>(`${URL}/login/`, data)
+        .pipe(catchError((error:HttpErrorResponse) => {
+            let err:string = '';
+            if(error.error.non_field_errors){
+                err = 'Invalid Credentials';
+            }
+            return throwError(() => err);
+        }),
+        tap(respData => {
+            this.localStorageService.setDetails(respData.token, respData.expiry);
+        }));
+    }
+    logout(){
+        return this.http.post(`${URL}/logout/`,{})
+        .pipe(tap(respData => {
+            this.localStorageService.clearSession();
+        }))
     }
 }
