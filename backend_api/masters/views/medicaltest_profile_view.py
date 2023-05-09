f8174d5a74c14de2181b7c689d2e019e9d598a11
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-05-09 12:55:10 +0530

    medical test updated

--- backend_api/masters/views/medicaltest_profile_view.py
+++ backend_api/masters/views/medicaltest_profile_view.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, response, status
 from masters import models as master_model
 from masters import serializers as master_serializers
+from django.db import transaction, connection
 
 class MedicalTestProfileList(generics.ListCreateAPIView):
 
-    queryset= master_model.MedicalTestProfile.objects.filter(is_deleted=False).order_by('-id')
+    queryset= master_model.MedicalTestProfile.objects.all().order_by('-id')
     serializer_class= master_serializers.MedicalTestProfileSerializer
 
 
@@ -13,10 +14,22 @@ class MedicalTestProfileDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = master_serializers.MedicalTestProfileSerializer
 
     def put(self, request, *args, **kwargs):
+        tests= master_model.MedicalTest.objects.filter(profile=request.data['id'])
+        tests.update(is_deleted=request.data['is_deleted'])
         return super().put(request, *args, **kwargs)
     
+    @transaction.atomic
     def patch(self, request, *args, **kwargs):
-        return super().patch(request, *args, **kwargs)
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        tests= master_model.MedicalTest.objects.filter(profile=request.data['id'])
+        tests.update(is_deleted=request.data['is_deleted'])
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 class MedicalTestProfileListWithoutPagination(generics.ListAPIView):
