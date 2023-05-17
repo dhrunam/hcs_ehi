a93e77ed345e8068134342afb0689448eab5986e
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-05-17 11:56:26 +0530

    migration recreated

--- frontend/src/app/dashboard/operations/employee/employee.service.ts
+++ frontend/src/app/dashboard/operations/employee/employee.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { URL } from 'src/environment/environment';
-
 @Injectable({
   providedIn: 'root'
 })
-export class HealthRecordService {
-  constructor(private http: HttpClient) { }
-  save_draft(fd: FormData){
-    return this.http.post<any>(`${URL}/operation/health_profile_test`,fd);
+export class EmployeeService {
+
+  constructor(private http:HttpClient) { }
+
+  get_emplyee()
+  {
+    return this.http.get(`${URL}/`);
   }
 }
