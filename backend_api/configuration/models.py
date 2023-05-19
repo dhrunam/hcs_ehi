4db2c1252ba1c9099162cf075bf4b240b6a0090c
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-05-19 10:36:52 +0530

    Developed API for health test report

--- backend_api/configuration/models.py
+++ backend_api/configuration/models.py
@@ -7,6 +7,7 @@ class MedicalTestSession(models.Model):
     year= models.IntegerField()
     session=models.CharField(max_length=128,null=False)
     
+    
 class MedicalTestProfilePerSession(models.Model):
     medical_test_session=models.ForeignKey(MedicalTestSession,on_delete=models.CASCADE, null=False, related_name='medical_test_profile_per_session')
     medical_test_profile=models.ForeignKey(master_models.MedicalTestProfile, 
--- backend_api/operation/urls.py
+++ backend_api/operation/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     
     path('operation/health_test', op_views.EmpHealthTestDetailsList.as_view()),
     path('operation/health_test/<int:pk>', op_views.EmpHealthTestDetailDetails.as_view()),
+
+    path('operation/health_test/upload/report', op_views.EmpHealthTestReportsList.as_view()),
+    # path('operation/health_test/upload/report/<int:pk>', op_views.EmpHealthTestReportsDetails.as_view()),
+    
     
 ]
