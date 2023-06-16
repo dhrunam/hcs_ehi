5f7b54040034008960f8454844f695792fae38c6
djb <dhrubajyoti.nic@gmail.com>
2023-06-16 16:46:57 +0530

    Side menu added

--- backend_api/masters/models.py
+++ backend_api/masters/models.py
@@ -48,12 +48,13 @@ class Employee(models.Model):
     designation = models.ForeignKey(Designation, null=True, on_delete= models.SET_NULL, related_name='employee')
     organisation = models.ForeignKey(Organisation, null=True, on_delete=models.SET_NULL, related_name='employee')
     name =  models.CharField(max_length=128, null=False)
-    blood_group = models.CharField(max_length=4, null=False)
+    blood_group = models.CharField(max_length=6, null=False)
     residenntial_address = models.CharField(max_length=1028, null= True, default='')
     date_of_birth = models.DateField(auto_created=False, auto_now= False)
     date_of_joining = models.DateField(auto_created=False, auto_now=False)
     date_of_superannuation = models.DateField(auto_created=False, auto_now= False)
     type = models.CharField(max_length=20, null= True, default='')
+    gender = models.CharField(max_length=20, null= True, default='')
 
 class Section(models.Model):
     name = models.CharField(max_length=50, null=False)
